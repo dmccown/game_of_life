3c3832f2f7b768f1500c1623488556d06e1a9ad5
David McCown <dmccown@gmail.com>
2013-07-04 11:44:10 -0500

    added executable

---
+++ bin/simluation.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+lib = File.expand_path(File.dirname(__FILE__) + '/../lib')
+$LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
+
+require 'board'
+require 'cell'
+require 'game_of_life'
+
+args = ARGV.dup
+ARGV.clear
+
+GameOfLife.run(args)
--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -2,12 +2,11 @@ require 'game_of_life/version'
 require 'board'
 
 module GameOfLife
-  def self.run
-    board = GameOfLife::Board.new 4, 4
+  def self.run(args)
+    board = GameOfLife::Board.new args[0].to_i, args[1].to_i
     board[1][1] = GameOfLife::Cell.new true
     board[1][2] = GameOfLife::Cell.new true
-    board[2][1] = GameOfLife::Cell.new true
-    board[2][2] = GameOfLife::Cell.new true
+    board[1][3] = GameOfLife::Cell.new true
 
     (0..100).each do | index |
       puts "#{index} ============== #{index}"
