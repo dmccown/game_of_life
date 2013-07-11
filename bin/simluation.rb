f9d9d41c3cf455d2d6fa82f1edcc1c7ed4d36dad
David McCown <dmccown@gmail.com>
2013-07-10 23:30:22 -0500

    Cleaned up output

--- bin/simluation.rb
+++ bin/simluation.rb
@@ -13,8 +13,7 @@ ARGV.clear
 system('clear')
 
 GameOfLife.run(args[0].to_i, args[1].to_i, args[2].to_f, args[3].to_i) do | output, index |
-  puts "#{index}"
   puts output
-  sleep 0.3
+  sleep 0.1
   system('clear')
 end
